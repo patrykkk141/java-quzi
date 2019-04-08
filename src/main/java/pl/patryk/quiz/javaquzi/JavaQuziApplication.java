493d126d6e5cb7a20ac2eb73d0737b1df25f8395
patrykkk141 <patryk.iwanczyk0102@gmail.com>
2019-04-08 19:46:58 +0200

    Setup project

---
+++ src/main/java/pl/patryk/quiz/javaquzi/JavaQuziApplication.java
@@ -0,0 +1,13 @@
+package pl.patryk.quiz.javaquzi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaQuziApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaQuziApplication.class, args);
+	}
+
+}
