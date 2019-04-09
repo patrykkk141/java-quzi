4305daad6fcf0edead770a30ab25cb635f8fddc5
patrykkk141 <patryk.iwanczyk0102@gmail.com>
2019-04-09 23:28:50 +0200

    changeed sample data and mysql driver class name

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,11 @@
-INSERT INTO `user`
-VALUES (1, 'abc', 'ADMIN', 'patryk'),
-       (2, 'abc', 'USER', 'marcin');
+INSERT INTO `user`(id, `password`, role, username, email, registration_date, enabled)
+VALUES (1, '{bcrypt}$2a$04$895j3QqSIaZU1dt62X4ntepX6E8XFiGfshM6ghGUG3WXVM/gaQkmO', 'ADMIN', 'patryk', 'patryk@example.com', '2019-04-09 18:04:59', true),
+       (2, 'abc', 'USER', 'marcin', 'marcin@example.com', '2015-03-11 11:13:51', true);
 
 INSERT INTO `question`
 VALUES (1, null, 'Która składnia dodawania obiektu panelu (JPanel panel = new JPanel();) do kontenera okna (JFrame frame = new JFrame();) w języku Java jest poprawna'),
-       (2, null, 'W języku Java rozkład komponentów (przycisków) w oknie aplikacji jak na rysunku niżej realizuje klasa (równa siatka przycisków 3x2 tej samej wielkości)');
+       (2, null, 'W języku Java rozkład komponentów (przycisków) w oknie aplikacji jak na rysunku niżej realizuje klasa (równa siatka przycisków 3x2 tej samej wielkości)'),
+       (3, null, 'óżźćąęśł');
 
 INSERT INTO `answer`
 VALUES (1,1, 'frame.add(panel);',1),
